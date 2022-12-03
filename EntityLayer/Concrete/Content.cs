831126d7d4414084ad2f45888c5d1789d10dab6f
Uğur TAŞLI <ugurtasli@outlook.com>
2022-12-03 22:35:36 +0300

    Context Sınıfı ve Web Config veri Tabanı bağlantı kodunun eklenmesi

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,19 @@ namespace EntityLayer.Concrete
 {
     public class Content
     {
+        [Key]
         public int ContentID { get; set; }
+        [StringLength(1000)]
         public string ContentValue { get; set; }
         public DateTime ContentDate { get; set; }
-        //ContentYazar
-        //Content Başlık
+        
 
         //Heading -Content İlişkisi
         public int HeadingID { get; set; }
         public virtual Heading Heading { get; set; }
+
+        //Yazar -Content İlişkisi
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
     }
 }
--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,23 @@ namespace EntityLayer.Concrete
 {
     public class Writer
     {
+        [Key]
         public int WriterID { get; set; }
+        [StringLength(50)]
         public string WriterName { get; set; }
+        [StringLength(50)]
         public string WriterSurname { get; set; }
+        [StringLength(100)]
         public string WriterImage { get; set; }
+        [StringLength(50)]
         public string WriterMail { get; set; }
+        [StringLength(20)]
         public string WriterPassword { get; set; }
 
         //Yazar -Başlık İlişkisi
         public ICollection<Heading> Headings { get; set; }
+        //Yazar -Content İlişkisi
+        public ICollection<Content> Contents { get; set; }
 
     }
 }
