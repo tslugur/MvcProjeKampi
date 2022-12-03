20bc2d65294eedc09523afa52324084086b7bb1d
Uğur TAŞLI <ugurtasli@outlook.com>
2022-12-03 22:02:15 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentID { get; set; }
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        //ContentYazar
+        //Content Başlık
+
+        //Heading -Content İlişkisi
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        public int HeadingID { get; set; }
+        public string HeadingName { get; set; }
+        public DateTime HeadingDate { get; set; }
+
+        //Category-Heading İlişkisi
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+        //Heading -Content İlişkisi
+        public ICollection<Content> Contents { get; set; }
+
+    }
+}
